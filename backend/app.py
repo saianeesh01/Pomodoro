bb559f347f600120639259f27fb1d37130e1fd4f
saianeesh01 <saianeesh01@gmail.com>
2025-02-15 23:12:27 -0500

    added port

--- backend/app.py
+++ backend/app.py
@@ -38,4 +38,5 @@ def serve(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     port = int(os.environ.get('PORT', 5000))  # Use Render's port or default to 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
